ad6e312f7bbcda783609034158961e5b16b11a21
Matt Harris <mharris@parallel.ai>
2026-01-23 18:11:49 -0500

    fix: remove parallel_enrich_company convenience function, keep only generic parallel_enrich

--- parallel_web_tools/integrations/bigquery/sql/create_functions.sql
+++ parallel_web_tools/integrations/bigquery/sql/create_functions.sql
@@ -20,30 +20,3 @@ OPTIONS (
     endpoint = '{function_url}',
     user_defined_context = [("processor", "lite-fast")]
 );
-
--- Convenience function for company enrichment
-CREATE OR REPLACE FUNCTION `{project_id}.{dataset_id}.parallel_enrich_company`(
-    company_name STRING,
-    company_website STRING,
-    fields STRING
-)
-RETURNS STRING
-AS (
-    `{project_id}.{dataset_id}.parallel_enrich`(
-        JSON_OBJECT('company_name', company_name, 'website', company_website),
-        fields
-    )
-);
-
--- Example queries:
---
--- SELECT parallel_enrich(
---     JSON_OBJECT('company_name', 'Google', 'website', 'google.com'),
---     JSON_ARRAY('CEO name', 'Founding year', 'Brief description')
--- );
---
--- SELECT parallel_enrich_company(
---     'Apple',
---     'apple.com',
---     JSON_ARRAY('CEO name', 'Market cap', 'Industry')
--- );
