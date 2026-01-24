020eeba24ab4e2b54c374668e8cba6f4d68c89a4
Matt Harris <mharris@parallel.ai>
2026-01-24 02:10:43 -0500

    bump to 0.0.5

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -112,7 +112,7 @@ class TestMainCLI:
         """Should show version."""
         result = runner.invoke(main, ["--version"])
         assert result.exit_code == 0
-        assert "0.0.4" in result.output
+        assert "0.0.5" in result.output
 
 
 class TestAuthCommand:
